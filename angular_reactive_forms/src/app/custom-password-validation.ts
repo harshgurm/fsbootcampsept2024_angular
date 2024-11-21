0bf6c7b1f713ad2115fcfc61405065609167b0a4
kaur6252 <harshgurm@gmail.com>
2024-11-20 21:58:00 -0500

    worked on reactive forms

---
+++ angular_reactive_forms/src/app/custom-password-validation.ts
@@ -0,0 +1,10 @@
+import { AbstractControl } from "@angular/forms";
+
+export function ValidatePassword(control:AbstractControl){
+    if(control.value.toUpperCase() == 'PASSWORD'){
+        return {
+            weakPassword: true
+        }
+    }
+    return null;
+}
---
+++ angular_reactive_forms/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css'
+})
+export class ReactiveFormComponent {
+
+  employeeForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    first_name: new FormControl('', [Validators.required]),
+    last_name: new FormControl(''),
+    salary: new FormControl(''),
+    department: new FormControl('', [Validators.required])
+  })
+
+  addEmployee(){
+    console.log(this.employeeForm);
+    return false;
+  }
+}
